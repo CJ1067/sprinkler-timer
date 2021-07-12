791197b306f94d05ff2fdb2cd8b8f3dae5561b73
CJ1067 <38930150+CJ1067@users.noreply.github.com>
2021-07-11 23:24:33 -0700

    Add main files and valve run times

---
+++ valve/scripts.js
@@ -0,0 +1,184 @@
+(function () {
+    let mask = document.getElementById("mask")
+    mask.setAttribute("visibility", "hidden")
+    loadTable(0);
+    let aButton = document.getElementById("a");
+    let bButton = document.getElementById("b");
+    let cButton = document.getElementById("c");
+    let aRun = document.getElementById("arun");
+    let bRun = document.getElementById("brun");
+    let cRun = document.getElementById("crun");
+    let saveButton = document.getElementById("save");
+    aButton.addEventListener("click", () => {
+        aButton.style.backgroundColor = "grey";
+        bButton.style.backgroundColor = "white";
+        cButton.style.backgroundColor = "white";
+        loadTable(0);
+    });
+    bButton.addEventListener("click", () => {
+        bButton.style.backgroundColor = "grey";
+        aButton.style.backgroundColor = "white";
+        cButton.style.backgroundColor = "white";
+        loadTable(1);
+    });
+    cButton.addEventListener("click", () => {
+        cButton.style.backgroundColor = "grey";
+        aButton.style.backgroundColor = "white";
+        bButton.style.backgroundColor = "white";
+        loadTable(2);
+    });
+    aRun.addEventListener("click", () => {
+        aRun.style.backgroundColor = "grey";
+        setTimeout(() => {  aRun.style.backgroundColor = "white"; }, 2000);
+        let url='http://98.167.209.219:8080/mp?pw=a6d82bced638de3def1e9bbb4983225c&pid=0';
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((result) => {
+
+            });
+    });
+    bRun.addEventListener("click", () => {
+        bRun.style.backgroundColor = "grey";
+        setTimeout(() => {  bRun.style.backgroundColor = "white"; }, 2000);
+        let url='http://98.167.209.219:8080/mp?pw=a6d82bced638de3def1e9bbb4983225c&pid=0';
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((result) => {
+                
+            });
+    });
+    cRun.addEventListener("click", () => {
+        cRun.style.backgroundColor = "grey";
+        setTimeout(() => {  cRun.style.backgroundColor = "white"; }, 2000);
+        let url='http://98.167.209.219:8080/mp?pw=a6d82bced638de3def1e9bbb4983225c&pid=0';
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((result) => {
+                
+            });
+    });
+    saveButton.addEventListener("click", () => {
+       
+        program = 0;
+        if (bButton.style.backgroundColor === "grey") {
+            program = 1;
+        } else if (cButton.style.backgroundColor === "grey") {
+            program = 2;
+        }
+
+        const url='http://98.167.209.219:8080/jp?pw=a6d82bced638de3def1e9bbb4983225c';
+
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((result) => {
+                data = result.pd[program]
+                let table = document.getElementById("valvetable")
+                for (var i = 1, row; row = table.rows[i]; i++) {
+                    let minsec = row.cells[1].childNodes[0].innerHTML.split(":");
+                    data[4][i - 1] = parseInt(minsec[0]) * 60 + parseInt(minsec[1])
+                }
+
+                var rowCount = table.rows.length;
+                for (var i = 1; i < rowCount; i++) {
+                    table.deleteRow(1);
+                }
+                let mask = document.getElementById("mask")
+                mask.innerHTML = "Saving..."
+                mask.style.visibility = "visible"
+
+                
+                var saveURL = 'http://98.167.209.219:8080/cp?pw=a6d82bced638de3def1e9bbb4983225c&pid=' + program + '&v=[';
+                saveURL += data[0] + "," + data[1] + "," + data[2] + ","
+                saveURL += "[" + data[3] + "]," + "[" + data[4] + "]]" 
+                console.log(saveURL)
+                fetch(saveURL)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((result) => {
+                            loadTable(program, false)
+                        });
+            });
+        });
+}())
+
+function loadTable (program, save = true) {
+    if (save) {
+        let table = document.getElementById("valvetable")
+        var rowCount = table.rows.length;
+        for (var i = 1; i < rowCount; i++) {
+            table.deleteRow(1);
+        }
+
+        let mask = document.getElementById("mask")
+        mask.innerHTML = "Loading..."
+        mask.style.visibility = "visible"
+    }
+    
+    const url='http://98.167.209.219:8080/jp?pw=a6d82bced638de3def1e9bbb4983225c';
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            durs = result.pd[program][4]
+            let table = document.getElementById("valvetable")
+            let mask = document.getElementById("mask")
+            mask.style.visibility = "hidden"
+
+            for (var i = 1; i <= 8; i++) {
+                let row = table.insertRow();
+
+                let valve = row.insertCell(0);
+                valve.innerHTML = i
+
+                let runtime = row.insertCell(1);
+                var editTime = document.createElement("div");
+                editTime.setAttribute("contenteditable", "");
+                let time = durs[i - 1]
+                editTime.innerHTML = Math.floor(time/60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":" + (time % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+                runtime.appendChild(editTime)
+
+                let manualtime = row.insertCell(2);
+                var editCell = document.createElement("div");
+                editCell.setAttribute("contenteditable", "");
+                editCell.setAttribute("id", "edit" + i);
+                editCell.innerHTML = "00:00"
+                manualtime.appendChild(editCell);
+
+                let manualrun = row.insertCell(3);
+                var manualButton = document.createElement("button");
+                manualButton.setAttribute("id", "manual" + i);
+                manualButton.innerHTML = "X";
+                this.manualListener = function(event) {
+                    var editValue = document.getElementById("edit" + this.id.slice(-1));
+                    let minsec = editValue.innerHTML.split(":");
+                    let time = parseInt(minsec[0]) * 60 + parseInt(minsec[1])
+                    console.log(time);
+                    let url='http://98.167.209.219:8080/cm?pw=a6d82bced638de3def1e9bbb4983225c&sid=' + this.id.slice(-1) + '&en=1&t=' + time;
+                    fetch(url)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((result) => {
+                            console.log(result)
+                        });
+                }
+                manualButton.addEventListener("click", this.manualListener, false);
+                manualrun.appendChild(manualButton);
+            }
+
+        });
+    
+
+}
